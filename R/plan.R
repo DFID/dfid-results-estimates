148562e1b7dd6ee4486f5a8eb1deede1e1cae37a
Charles Baillie <c-baillie@dfid.gov.uk>
2020-05-15 17:00:07 +0100

    added read funcs.

--- R/plan.R
+++ R/plan.R
@@ -1,17 +1,30 @@
 the_plan <-
   drake_plan(
 
-## load data
+## load template data
+# remote
 education_raw <- readEducation("Z:"),
 family_raw <- readFamily("Z:"),
 humanitarian_raw <- readHumanitarian("Z:"),
 nutrition_raw <- readNutrition("Z:"),
 pfm_raw <- readPFM("Z:"),
 a2f_raw <- readA2F("Z:"),
+#dept_raw <- readDeptData("Z:")
+# local
+jobs <- read_csv("jobs.csv"),
+multilat <- read_csv("data/multilat.csv"),
+lookup <- read_csv("data/dept_lookup.csv"),
 
 ## format data
+education_tidy <- tidyEducation("Z:"),
+family_tidy <- tidyFamily("Z:"),
+humanitarian_tidy <- tidyHumanitarian("Z:"),
+nutrition_tidy <- tidyNutrition("Z:"),
+pfm_tidy <- tidyPFM("Z:"),
+a2f_tidy <- tidyA2F("Z:"),
 
 ## cat data
+master <- bind_rows(education_tidy, family_tidy, humanitarian_tidy, nutrition_tidy, pfm_tidy, a2f_tidy, jobs, multilat),
 
 ## filter data
 
