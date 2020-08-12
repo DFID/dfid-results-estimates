3d604d2fb039631d7eab1574aa7a85f5228ca6dd
charlesbaillie <c-baillie@dfid.gov.uk>
2020-08-12 11:05:23 +0100

    tidy up

--- R/packrat_setup.R
+++ R/packrat_setup.R
@@ -1,60 +1,66 @@
-# if(!require("packrat")){
-#   install.packages("packrat", repos = "https://cran.rstudio.com/")
-#   library(remotes)
-# }
-#
-# packrat::init(
-#   infer.dependencies = FALSE,
-#   enter = TRUE,
-#   restart = FALSE)
-#
-# ### Setup repositories ###
-#
-# # Install packages that install packages.
-# install.packages("remotes", repos = "https://cran.rstudio.com/")
-#
-# # Set repos.
-# my_repos <- vector()
-# my_repos["CRAN"] <- "https://cran.rstudio.com/"
-# options(repos = my_repos)
-#
-# ### Install CRAN packages ###
-# cran_packages <- c(
-#   "conflicted",
-#   "dplyr",
-#   "dotenv",
-#   "drake",
-#   "ggplot2",
-#   "patchwork",
-#   "here",
-#   "janitor",
-#   "knitr",
-#   "lubridate",
-#   "magrittr",
-#   "openxlsx",
-#   "scales",
-#   "stringr",
-#   "tidyr",
-#   "readr",
-#   "zoo"
-# )
-#
-# install.packages(cran_packages)
-#
-# ### Install github packages ###
-# github_packages <- c(
-#   "ukgovdatascience/govstyle"
-# )
-#
-# remotes::install_github(github_packages)
-#
-# # gt needs to be installed at a specific commit to deal with a bug
-# # https://github.com/rstudio/gt/issues/280
+if(!require("packrat")){
+  install.packages("packrat", repos = "https://cran.rstudio.com/")
+   library(remotes)
+ }
+
+packrat::init(
+   infer.dependencies = FALSE,
+   enter = TRUE,
+   restart = FALSE)
+
+#### Setup repositories ----
+
+# Install packages that install packages.
+install.packages("remotes", repos = "https://cran.rstudio.com/")
+
+# Set repos.
+my_repos <- vector()
+my_repos["CRAN"] <- "https://cran.rstudio.com/"
+options(repos = my_repos)
+
+#### Install CRAN packages ----
+ cran_packages <- c(
+"conflicted",
+"dplyr",
+"dotenv",
+"drake",
+"ggplot2",
+"govstyle",
+"patchwork",
+"purrr",
+"here",
+"janitor",
+"knitr",
+"lubridate",
+"magrittr",
+"openxlsx",
+"scales",
+"stringr",
+"tidyr",
+"readr",
+"forcats",
+"zoo",
+"rnaturalearth",
+"rnaturalearthdata",
+"ggrepel",
+"sf"
+)
+
+install.packages(cran_packages)
+
+#### Install github packages ----
+ github_packages <- c(
+   "ukgovdatascience/govstyle"
+ )
+
+remotes::install_github(github_packages)
+
+# If using, gt needs to be installed at a specific commit to deal with a bug to work with latex: https://github.com/rstudio/gt/issues/280
 # remotes::install_github("rstudio/gt", ref = "51a812ba6a10769bd24e01c82a3e1b7de44a5a40")
-#
-# ### Take snapshot ###
-#
-# packrat::snapshot(
-#   snapshot.sources = FALSE,
-#   ignore.stale = TRUE,
-#   infer.dependencies = FALSE)
+
+#### Take snapshot ----
+
+packrat::snapshot(
+   snapshot.sources = FALSE,
+   ignore.stale = TRUE,
+   infer.dependencies = FALSE)
