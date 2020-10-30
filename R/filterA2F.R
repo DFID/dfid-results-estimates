4db81b1869fda18a5a81a7ace940bc6214298284
JosephCrispell <crispelljoseph@gmail.com>
2020-10-30 15:56:51 +0000

    slice_tail() by default extracts last row

--- R/filterA2F.R
+++ R/filterA2F.R
@@ -7,15 +7,15 @@
 filterA2F  <-  function(data){
 
   data %>%
-    rowwise() %>%
-    mutate(not_identified = total-sum(male,female, na.rm=T)) %>%
-    ungroup()  %>%
-    mutate_at(vars(not_identified),function(x)ifelse(x<=1,0,x)) %>%
-    adorn_totals("row") %>%
-    select(department,  female, male,  not_identified,  total)  %>%
-    slice_tail(n()) %>%
-    select(-department) %>%
-    mutate_if(is.numeric, roundChoose, 100000) %>%
-    clean_names("title")
+     rowwise() %>%
+     mutate(not_identified = total-sum(male,female, na.rm=T)) %>%
+     ungroup()  %>%
+     mutate_at(vars(not_identified),function(x)ifelse(x<=1,0,x)) %>%
+     adorn_totals("row") %>%
+     select(department,  female, male,  not_identified,  total)  %>%
+     slice_tail() %>%
+     select(-department) %>%
+     mutate_if(is.numeric, roundChoose, 100000) %>%
+     clean_names("title")
 
 }
